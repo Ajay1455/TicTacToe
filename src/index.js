66a0e547b2705ed7c9ad97ccc4f26e01f81b5b28
Ajay1455 <ajay1455.be20@chitkara.edu.in>
2023-04-11 16:12:17 +0530

    add some footer styling

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./Game";
+
+const mystyle = {
+  color: "white",
+  backgroundColor: "DodgerBlue",
+  padding: "10px",
+  fontFamily: "Arial",
+  textAlign: "center",
+  top: "50px",
+  postion: "absolute",
+  margin: "0",
+};
+
+const ele = (
+  <div style={{margin:"0"}}>
+    <h1 style={mystyle}>Tic - Tac - Toe</h1>
+    <Game />
+    <p style={{ width: "100%", textAlign: "center", color: "grey" , position:"absolute", top:"700px"}}>
+      Make By Ajay❤️
+    </p>
+  </div>
+);
+
+let root = document.getElementById("root");
+ReactDOM.render(ele, root);
